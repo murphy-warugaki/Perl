0128b96d15f12b72f39a46307c6efaad5358a128
murphy-warugaki <murphydayo120@gmail.com>
2016-06-24 19:18:52 +0900

    modified replacement

--- replacement.pl
+++ replacement.pl
@@ -5,10 +5,15 @@ use utf8;
 open(my $in, "<", "test.dat") or die("could  not open file.");
 
 while(<$in>){ 
-$_ =~ s/abc/ABC/;
-if ($_ =~ /[a-z]/){
-print $_;
-}
+	s/abc/ABC/; #$_ =~省略できる
+
+	s/(abc|def)/\U$1/; #Uは大文字に変換
+
+	s/(DEF|GHI)/\L/; #Lは小文字に変換
+
+	if (/[A-Z]/){
+		print $_;
+	}
 }
 
 close($in);
