28f45a0ab8aa33ae4cd740ab5b2af3d9b7da33bd
murphy-warugaki <shinya.nishimoto@mkt-apps.com>
2016-07-14 19:50:11 +0900

    a

--- Cpan/code/Test/t/text-csv_xs.pl
+++ Cpan/code/Test/t/text-csv_xs.pl
@@ -7,7 +7,7 @@ use Data::Dumper;
 my $class = 'Text::CSV_XS';
 use_ok $class;
 
-subtest 'scv' => sub {
+subtest 'example' => sub {
 
     #my $csv = Text::CSV_XS->new({ binary => 1, always_quote => 1, sep_char => "\t", });
     #my $test = $csv->say(*STDOUT, [qw(id, name)]);
@@ -17,12 +17,12 @@ subtest 'scv' => sub {
     my $sample_input_string = '"I said, ""Hi!""",Yes,"",2.34,,"1.09"';
     if ($csv->parse($sample_input_string)) {
         my @fields = $csv->fields;
-warn Dumper @fields;
         my $count  = 0;
         for $column (@fields) {
             print ++$count, "=>", $column, "\n";
         }
         print "\n";
     }
+    ok $csv;
 };
 done_testing;
