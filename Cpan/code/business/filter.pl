b166d216faea958f082d2c691ce4d909002f4230
murphy-warugaki <murphydayo120@gmail.com>
2016-08-12 10:48:32 +0900

    add filter.pl

--- Cpan/code/business/filter.pl
+++ Cpan/code/business/filter.pl
@@ -1,27 +1,31 @@
 use common::sense;
 use Data::Dumper;
 
+#methodを実装していく
+#forで探したいキーを回して一致したらハッシュにいれてリファレンスにして返す
 sub filter_keys {
-my ($data, $keys) = @_;
-my %result;
-for my $key (@$keys){
-$result{$key} = $data->{$key}
-if exists $data->{$key};
-}
-\%result;
+	my ($data, $keys) = @_;
+	my %result;
+	for my $key (@$keys){
+		$result{$key} = $data->{$key}
+		if exists $data->{$key};
+	}
+	\%result;
 }
 
+#JSON dataを変数にして呼び出す
 my $data = {
-hoge => 'hoge',
-fuga => 'Rfuga',
-Roge => 'fooo',
-bara => 'barr',
-accept_email => 1,
+	hoge => 'hoge',
+	fuga => 'Rfuga',
+	Roge => 'fooo',
+	bara => 'barr',
+	accept_email => 1,
 };
 
-my @my_keys = qw( accept_email hog);
+#どのようなキーならフィルターを通るのか@にして書く
+my @my_keys = qw(accept_email hog);
 
-
+#アクセサーを書く(@をリファレンス化して渡す)
 my $result = filter_keys($data, \@my_keys);
 
 warn Dumper $result;
