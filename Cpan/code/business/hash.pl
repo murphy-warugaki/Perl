2fcaa84d24f66dcfb041e4ad05ac08d4b334b687
murphy-warugaki <shinya.nishimoto@mkt-apps.com>
2016-09-15 16:37:30 +0900

    a

--- Cpan/code/business/hash.pl
+++ Cpan/code/business/hash.pl
@@ -7,19 +7,24 @@ my %names;
 # how to set in the hash
 # 01
 %names = (
-    murphy => 150,
-    nishimoto    => 220,
+    murphy    => 150,
+    nishimoto => 220,
 );
 
-# 02
-$names{shinya} = '560';
+# 02 代入
+my %nickname;
+$nickname{shinya} = '560';
 
-# how to access to hash
-foreach my $key ( keys %names ) {
-    print "My name is $key has $names{$key}\n";
+# how to access to hash 参照
+# we need () in for loop. ()は必要 
+for (keys %names) {
+    print "My name is $_ . He has $names{$_}\n";
 }
 
-my %values = ("today" => 100, "yesterday" => 200, "tomorrow" => 300);
-for (keys %values) {
-    print "temputure is $values{$_} and $_\n";
-}
+# 3 ) 値の取得
+my @values = values %names; # 150 220
+
+# 4 ) キーの存在確認
+@values = %nickname if exists $nickname{shinya}; # values は上書きされる
+
+delete $names{murphy}; # murpshy が消えてnishimoto だけが残る
